9c92fb47a8952a31726abbddb8487bcb270ee992
Savecoders <pablopincay2003@gmail.com>
2023-10-29 22:50:11 -0500

    create new endpoints

--- api/routes/cnelPowerOutages.routes.js
+++ api/routes/cnelPowerOutages.routes.js
@@ -6,18 +6,62 @@ import CnelPoweroutagesService from "../services/CnelPowerOutages.service.js";
 // validation handlers
 
 // services
-
 const router = Router();
 const cnelPoweroutagesService = new CnelPoweroutagesService();
 
 await cnelPoweroutagesService.setScrapeData();
 
-// return all data using json format
-
+// endpoints
+// return all data
 router.get("/", async (req, res, next) => {
   try {
     // get data from service
     const data = cnelPoweroutagesService.data;
+    res.status(200).json({
+      data: data,
+      message: "data listed",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// return data filtered by location
+router.get("/:location", async (req, res, next) => {
+  const { location } = req.params;
+  try {
+    // get data from service
+    const data = cnelPoweroutagesService.findLocation(location);
+    res.status(200).json({
+      data: data,
+      message: "data listed",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// return data filtered by location and sector
+router.get("/:location/:sector", async (req, res, next) => {
+  const { location, sector } = req.params;
+  try {
+    // get data from service
+    const data = cnelPoweroutagesService.findData(location, sector);
+    res.status(200).json({
+      data: data,
+      message: "data listed",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// return data filtered by location and time
+router.get("/:location/time/:time", async (req, res, next) => {
+  const { location, time } = req.params;
+  try {
+    // get data from service
+    const data = cnelPoweroutagesService.filterLocationTime(location, time);
     res.status(200).json({
       data: data,
       message: "data listed",
--- api/services/CnelPowerOutages.service.js
+++ api/services/CnelPowerOutages.service.js
@@ -14,4 +14,46 @@ export default class CnelPoweroutagesService {
   get data() {
     return this.data;
   }
+
+  findLocation(location) {
+    return this.data.find((item) => {
+      return item.location === location.toLowerCase();
+    });
+  }
+
+  findData(location, sector) {
+    const { power_outages, pdf_url } = this.findLocation(location);
+    const power_outage = power_outages.find(({ sectors }) => {
+      return sectors.includes(sector.toLowerCase());
+    });
+
+    if (power_outage.length === 0) return;
+
+    const { time } = power_outage;
+
+    return {
+      pdf_url,
+      location,
+      time,
+      sector,
+    };
+  }
+
+  filterLocationTime(location, timeRange = "12-14") {
+    let { power_outages, pdf_url } = this.findLocation(location);
+
+    power_outages = power_outages.filter(({ time }) => {
+      const startTime = time.substring(0, 2);
+      const endTime = time.substring(7, 9);
+      const timeRangeStart = timeRange.substring(0, 2);
+      const timeRangeEnd = timeRange.substring(3, 5);
+      return +startTime >= +timeRangeStart && +endTime <= +timeRangeEnd;
+    });
+
+    return {
+      pdf_url,
+      location,
+      power_outages,
+    };
+  }
 }
